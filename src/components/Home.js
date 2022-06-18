f7896a540c360e4b0e5b594ed36997887e0ca3fd
DMTA <100156884+Dumblevor@users.noreply.github.com>
2022-06-18 19:06:03 +0300

    start2

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,18 +24,22 @@ function Home() {
 
 
   return (
-    <section className="hero is-fullheight-with-navbar is-warning">
-      <div className="hero-body">
-        <div className="container">
-          <h1 className="title is-1 has-text-black">
-            Welcome to Hacker Trees splash page
-          </h1>
-          <button className="button is-large is-primary is-light">Register</button> <br /> <br />
-          <button className="button is-large is-primary is-light">Login</button>
-          <div className="hero  ">
-            <img className="image is-large" src={splashImage}></img>
+    <section classNameName="hero is-warning">
+
+      <form className="container column box is-one-third">
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control">
+            <input className="input" type="email" placeholder="e.g. alex@example.com" />
           </div>
         </div>
+
+        <button className="button is-secondary">Sign in</button>
+        <button className="button is-primary">Register</button>
+      </form>
+      <div className="column is-half">
+        <img classNameName="image" src={splashImage} />
+
       </div>
     </section>
   )

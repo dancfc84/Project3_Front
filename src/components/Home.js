2bb1e40eacb0d1d8af1f19f9528096a27914ffc1
codyshan <qiaute.shan@gmail.com>
2022-06-23 19:14:18 +0200

    Login-Register Frontend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,111 @@
-import React from "react"
 import splashImage from '../assets/malwareImage.jpg'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 // import { Link } from "react-router-dom"
 
 
 export default function Home() {
-  // function handleRegister() {
-  // }
+  const navigate = useNavigate()
 
-  // function hadnleLogin() {
-  // }
+  // ! Put our form fields in state.
+  const [formData, setFormData] = useState({
+    username: "Cody2",
+    password: "Cody2hello%8",
+    passwordConfirmation: "Cody2hello%8",
+    email: "cody2@frontend.com",
+  })
 
-  //cover image
-  //cover text content 
-  // email input
-  //register button 
-  //login button ? 
+  function handleChange(e) {
+    // ! name: field you've typed in, e.g. the email input
+    // ! value: the text that's in that field
+    const { name, value } = e.target
+    setFormData({
+      ...formData, // ! This is whatever the form data was before, all it's fields.
+      [name]: value, 
+    })
+  }
+
+  async function handleRegister(e) {
+    e.preventDefault()
+
+    try {
+      const { data } = await axios.post('/api/register', formData)
+      // ! Navigate to the /login page. 
+      navigate('/')
+
+    } catch (err) {
+      // ! Print out the response from the backend if there's an error
+      console.log(err.response.data)
+    }
+  }
+
+  async function handleLogin(e) {
+    e.preventDefault()
+    try {
+      const { data } = await axios.post('/api/login', formData)
+      localStorage.setItem('token', data.token)
+      console.log(data.token)
+      navigate('/')
+      // ! Navigate to the /login page. 
+    } catch (err) {
+      // ! Print out the response from the backend if there's an error
+      console.log(err.response.data)
+    }
+  }
+
 
 
   return (
     <section className="">
       <div className="container column box is-one-third">
-
-        <div className="field">
-          {/* <label className="label">Email</label> */}
-          <div className="control">
-            <input className="input" type="mail" placeholder="Email, e.g. 1@hackerone.com" />
+        <form>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                className="input"
+                type="text"
+                name={'email'}
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-        </div>
-        <div className="field is-grouped">
-          <div className="control">
-            <button className="button is-primary">Login</button>
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input
+                className="input"
+                type="password"
+                name={'password'}
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-          <div className="control">
-            <button className="button is-light">Register</button>
+          <div className="field">
+            <label className="label">Confirm password</label>
+            <div className="control">
+              <input
+                className="input"
+                type="password"
+                name={'passwordConfirmation'}
+                value={formData.passwordConfirmation}
+                onChange={handleChange}
+              />
+            </div>
           </div>
-        </div>
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-primary" onClick={handleLogin}>Login</button>
+            </div>
+            <div className="control">
+              <button className="button is-light" onClick={handleRegister}>Register</button>
+            </div>
+          </div>
+        </form>
       </div>
       <div className="columns">
         <div className="column is-one-thirds" />

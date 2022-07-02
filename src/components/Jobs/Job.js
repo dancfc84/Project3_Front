2ad362f265f33be61c2c0188b7f24d89900a1385
dancfc84 <danwhittock2013@gmail.com>
2022-07-02 12:27:03 +0100

    Last minute changes

--- src/components/Jobs/Job.js
+++ src/components/Jobs/Job.js
@@ -9,7 +9,7 @@ const Job = (props) => {
   return <div className={styles.card_margin}>
     <Link style={{ textDecoration: "none" }} to={`/jobs/${props.jobInfo._id}`}>
       <div className={`card ${styles.card_dimensions}` }>
-        <div className={`card-header ${styles.card_header_text}`}>
+        <div className={`card-header ${styles.card_header_container}`}>
           <div className={`card-header-title ${styles.card_header_text}`}>{props.jobInfo.jobTitle}</div>
         </div>
         <div className={`card-image ${styles.card_image_size}`}>

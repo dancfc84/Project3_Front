01fbef746d440970345c06c028c6fdf77970172c
dancfc84 <danwhittock2013@gmail.com>
2022-07-07 17:01:05 +0100

    REACT_APP_PROD_URL

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const devUrl = '/api'
+const prodUrl = "https://it-links.herokuapp.com/api"
 
-const prodUrl = process.env.REACT_APP_PROD_URL
 const baseUrl = process.env.NODE_ENV === 'production' ? prodUrl : devUrl
 
 export default baseUrl
